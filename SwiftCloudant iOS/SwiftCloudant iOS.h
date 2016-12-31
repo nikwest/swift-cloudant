198708c40995eddfa659dd828a6395a4a1ebfbfd
Dominik Westner <nikwest@github.com>
2016-12-31 19:57:23 +0100

    added iOS files

---
+++ SwiftCloudant iOS/SwiftCloudant iOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCloudant iOS.h
+//  SwiftCloudant iOS
+//
+//  Created by Dominik Westner on 31/12/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftCloudant iOS.
+FOUNDATION_EXPORT double SwiftCloudant_iOSVersionNumber;
+
+//! Project version string for SwiftCloudant iOS.
+FOUNDATION_EXPORT const unsigned char SwiftCloudant_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCloudant_iOS/PublicHeader.h>
+
+
